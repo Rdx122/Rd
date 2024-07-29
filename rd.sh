8882178c9c9b75ef7117f6e02ae6c9775603008b
vipul gohel <90172796+Rdx122@users.noreply.github.com>
2024-07-29 20:57:39 +0530

    Create rd.sh

---
+++ rd.sh
@@ -0,0 +1,9 @@
+Codes: https://github.com/dkbroyt/aman/blob/main/aman.sh
+
+git clone https://github.com/dkbroy
+
+cd rd.sh
+
+chmod +x rd.sh && ./rd.sh
+
+common: git add . && git commit -m "feat: initiated the project" && git push origin main
